30fe49dd11e9694178a82a928a8fd745f2134f65
Anton <@mail.com>
2024-09-23 02:12:40 +0300

    make script for swiping templates but not almost done

---
+++ scripts/swiping.js
@@ -0,0 +1,16 @@
+import { renderHtml } from './rendering'
+import { LinkedList } from './linked_list'
+
+list = new LinkedList()
+list.addAll([
+  '/forms/login/login.html',
+  '/forms/signup/signup.html',
+])
+
+export function swipeNext() {
+  
+}
+
+export function swipePrevious() {
+
+}

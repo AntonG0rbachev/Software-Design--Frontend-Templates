62b84b527a085398e8f1d6164474a2c53e785ee9
Anton <@mail.com>
2024-09-22 17:31:53 +0300

    make redering script

---
+++ scripts/rendering.js
@@ -0,0 +1,21 @@
+export function renderHtml(file, containerId) {
+  fetch(file)
+    .then(response => {
+      if (!response.ok) {
+        throw new ReferenceError(
+            `Could not render file: ${response.statusText}`
+          )
+      }
+      return response.text
+    })
+    .then(data => {
+      document
+        .querySelector(`#${containerId}`)
+        .innerHTML = data;
+    })
+    .catch(error => {
+      console.error(
+        'Loading error: ', error
+      )
+    })
+}

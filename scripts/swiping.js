8d824d294cf174f7f4ff477a2864e91c383836bc
Anton <@mail.com>
2024-09-23 13:54:22 +0300

    FINALLY complete swiping

--- scripts/swiping.js
+++ scripts/swiping.js
@@ -4,18 +4,22 @@ export function swipeNext(list, changingBlock) {
   if (list.getNext() == null) {
     console.error('next item missing')
   } else {
+    console.log(`before turn right:\n head is ${list.head().element}\n next is ${list.getNext().element}`);
     var nextForm = list.getNext();
     list.moveNext();
+    console.log(`after turn right:\n head is ${list.head().element}\n prev is ${list.getPrevious().element}`);
     renderHtml(nextForm.element, changingBlock)
   }
 }
 
 export function swipePrevious(list, changingBlock) {
-  if (list.getNext() == null) {
+  if (list.getPrevious() == null) {
     console.error('previous item missing')
   } else {
+    console.log(`before turn left:\n head is ${list.head().element}\n prev is ${list.getPrevious().element}`);
     var previousForm = list.getPrevious();
     list.movePrevious();
+    console.log(`after turn left: ${list.head().element}`);
     renderHtml(previousForm.element, changingBlock);
   }
 }
